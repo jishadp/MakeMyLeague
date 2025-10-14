e309f8344ab198c740391d42376e330d8d9a8b6f
mhdniyas <mhdniyas37@gmail.com>
2025-10-15 00:59:01 +0530

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         dateFormat: "Y-m-d",
         allowInput: true,
         enableTime: false,
-        minDate: "today",
         disableMobile: false, // Enable on mobile devices
         clickOpens: true,
         monthSelectorType: "static", // Fix month navigation
