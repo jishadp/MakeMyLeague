763be0794098442fe75734ba1ec1154c66621486
mhdniyas <mhdniyas37@gmail.com>
2025-10-17 23:16:04 +0530

    fix

--- resources/views/auction/index.blade.php
+++ resources/views/auction/index.blade.php
@@ -174,7 +174,7 @@
   src="https://code.jquery.com/jquery-3.7.1.js"
   integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
   crossorigin="anonymous"></script>
-<script src="{{ asset('js/auction.js') }}?v=14"></script>
+<script src="{{ asset('js/auction.js') }}?v=15"></script>
 <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 <script src="{{ asset('js/pusher-main.js') }}?v={{ time() + 1 }}"></script>
 
